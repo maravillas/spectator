784a6c29c45b3c8287cd52bf85cae1f9e76310af
Matthew Maravillas <maravillas@gmail.com>
2010-04-04 02:01:52 -0400

    Add core

--- src/spectator/core.clj
+++ src/spectator/core.clj
@@ -1,1 +1,42 @@
-(ns spectator.core)
+(ns spectator.core
+  (:require [spectator.multimap :as multimap])
+  (:use	[spectator.map-util]
+	[clojure.contrib swing-utils logging]))
+
+(defn- run-watchers [old-context context watchers]
+  (if (first watchers)
+    (recur old-context (merge context ((first watchers) old-context context)) (rest watchers))
+    context))
+
+(defn notify-watchers [context updates]
+  (let [old-context context
+	all-watchers (:watchers context) 
+	watchers (distinct (mapcat #(%1 all-watchers) (keys updates)))
+	new-context (run-watchers old-context context watchers)
+	diff (map-diff old-context new-context)]
+    (if (seq diff)
+      (recur new-context (select-keys new-context diff))
+      new-context)))
+
+(defn- redundant-update? [context key value]
+  (and (contains? context key)
+       (= (key context) value)))
+
+(defn update 
+  ([context key value]
+     (update context key value false))
+
+  ([context key value silent]
+     "Updates the context with a new value."
+     (let [new (assoc context key value)]
+       (cond
+	(redundant-update? context key value) context
+	silent new
+	true (notify-watchers new {key value})))))
+
+(defn watch-keys [context f & keys]
+  "Adds a watch to the context that is run only when the key's
+value changes. f should be a function taking two arguments: the
+context and the key's new value."
+  (let [kvs (conj (vec (interpose f keys)) f)]
+    (assoc context :watchers (apply multimap/add (:watchers context) kvs))))
--- test/spectator/core_test.clj
+++ test/spectator/core_test.clj
@@ -2,5 +2,80 @@
   (:use [spectator.core] :reload-all)
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false))
+(deftest updates-key
+  (let [context {}
+	context (update context :foo 1)]
+    (is (= (:foo context)
+	   1))))
+
+(deftest stops-cycles-when-values-are-unchanged
+  (let [context {:a 0 :b 3}
+	context (watch-keys context (fn [old new] {:a (max 0 (dec (:b new)))}) :b)
+	context (watch-keys context (fn [old new] {:b (max 0 (dec (:a new)))}) :a)
+	context (update context :a 3)]
+    (is (= (:a  context)
+	   0))
+    (is (= (:b context)
+	   0))))
+
+(deftest watches-not-run-when-no-change
+  (let [context {:foo 1}
+	context (watch-keys context (fn [old new] {:ran true}) :foo)
+	context (update context :foo 1)]
+    (is (not (:ran context)))))
+
+(deftest watches-key
+  (let [context {}
+	context (watch-keys context (fn [old new] {:ran true}) :foo)
+	context (update context :foo 1)]
+    (is (:ran context))))
+
+(deftest watches-multiple-keys-with-one-watcher
+  (let [context {:m 0 :n 0}
+	context (watch-keys context (fn [old new] {:max (max (:m new) (:n new))}) :m :n)
+	context (update context :m 1)]
+    (is (= (:max context)
+	   1))
+    
+    (let [context (update context :n 3)]
+      (is (= (:max context)
+	     3)))))
+
+(deftest watches-multiple-keys-with-multiple-watchers
+  (let [context {}
+	context (watch-keys context (fn [old new] {:next-i (inc (:i new))}) :i)
+	context (watch-keys context (fn [old new] {:next-j (dec (:j new))}) :j)
+	context (update context :i 3)
+	context (update context :j 8)]
+    (is (= (:next-i context)
+	   4))
+    (is (= (:next-j context)
+	   7))))
+
+(deftest watches-one-key-with-multiple-watchers
+  (let [context {}
+	context (watch-keys context (fn [old new] {:next-i (inc (:i new))}) :i)
+	context (watch-keys context (fn [old new] {:prev-i (dec (:i new))}) :i)
+	context (update context :i 3)]
+    (is (= (:next-i context)
+	   4))
+    (is (= (:prev-i context)
+	   2))))
+
+(deftest gives-latest-watcher-priority
+  (let [context {}
+	context (watch-keys context (fn [old new] {:j (inc (:i new))}) :i)
+	context (watch-keys context (fn [old new] {:j (dec (:i new))}) :i)
+	context (update context :i 3)]
+    (is (= (:j context)
+	   2))))
+
+(deftest cascades-watcher-chains
+  (let [context {}
+	context (watch-keys context (fn [old new] {:b (inc (:a new))}) :a)
+	context (watch-keys context (fn [old new] {:c (inc (:b new))}) :b)
+	context (update context :a 3)]
+    (is (= (:b context)
+	   4))
+    (is (= (:c context)
+	   5))))
