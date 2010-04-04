1bcd28514dbe5a352736b474f1b8934a64e20151
Matthew Maravillas <maravillas@gmail.com>
2010-04-03 23:10:46 -0400

    Add lein swank :dev-dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject spectator "1.0.0-SNAPSHOT"
   :description "A functional event system for Clojure maps."
   :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]])
+                 [org.clojure/clojure-contrib "1.1.0"]]
+  :dev-dependencies [[ant/ant "1.6.5"]
+                     [ant/ant-launcher "1.6.5"]
+                     [leiningen/lein-swank "1.2.0-SNAPSHOT"]])
