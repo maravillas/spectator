2c72b54a590d9e91e5d81f73c0c9981cd4cee625
Matthew Maravillas <maravillas@gmail.com>
2010-04-05 15:24:06 -0400

    Add log4j to project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject spectator "1.0.0-SNAPSHOT"
   :description "A functional event system for Clojure maps."
   :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]]
+                 [org.clojure/clojure-contrib "1.1.0"]
+		 [commons-logging/commons-logging "1.1"]]
   :dev-dependencies [[ant/ant "1.6.5"]
                      [ant/ant-launcher "1.6.5"]
                      [leiningen/lein-swank "1.2.0-SNAPSHOT"]])
