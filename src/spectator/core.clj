4fd8d9c12d769f9f464503ab4988579a33060bb8
Matthew Maravillas <maravillas@gmail.com>
2010-04-07 16:42:46 -0400

    Remove references to 'context' and replace them with 'map'

--- src/spectator/core.clj
+++ src/spectator/core.clj
@@ -3,89 +3,89 @@
   (:use [spectator.map-util]
 	[clojure.contrib swing-utils logging]))
 
-(defn- watchers [context]
-  (:watchers (meta context)))
+(defn- watchers [map]
+  (:watchers (meta map)))
 
-(defn- without-memo [context]
-  (with-meta context (dissoc (meta context) :memo)))
+(defn- without-memo [map]
+  (with-meta map (dissoc (meta map) :memo)))
 
-(defn- run-watchers [old-context updates watchers]
-  (let [new-context (merge-with-meta old-context updates)]
+(defn- run-watchers [old-map updates watchers]
+  (let [new-map (merge-with-meta old-map updates)]
     (if (first watchers)
-      (recur old-context
-	     (merge-with-meta updates ((first watchers) old-context new-context))
+      (recur old-map
+	     (merge-with-meta updates ((first watchers) old-map new-map))
 	     (rest watchers))
       updates)))
 
-(defn- watchers-for-keys [context keys]
-  (let [watchers (watchers context)]
+(defn- watchers-for-keys [map keys]
+  (let [watchers (watchers map)]
     (distinct (mapcat #(%1 watchers) keys))))
 
 (defn- notify-watchers
-  ([old-context updates]
-     (notify-watchers old-context updates updates))
+  ([old-map updates]
+     (notify-watchers old-map updates updates))
   
-  ([old-context updates all-updates]
-     (let [watchers     (watchers-for-keys old-context (keys updates))
-	   next-updates (run-watchers old-context updates watchers)
-	   new-context  (merge-with-meta old-context updates)
+  ([old-map updates all-updates]
+     (let [watchers     (watchers-for-keys old-map (keys updates))
+	   next-updates (run-watchers old-map updates watchers)
+	   new-map      (merge-with-meta old-map updates)
 	   diff         (with-meta (map-diff updates next-updates)
 			  (merge (meta updates) (meta next-updates)))]
        (if (not (map-subset? next-updates all-updates))
-	 (recur new-context diff (merge-with-meta all-updates next-updates))
+	 (recur new-map diff (merge-with-meta all-updates next-updates))
 	 all-updates))))
 
-(defn- redundant-update? [context key value]
-  (and (contains? context key)
-       (= (key context) value)))
+(defn- redundant-update? [map key value]
+  (and (contains? map key)
+       (= (key map) value)))
 
-(defn- alter-watches [context op f & keys]
+(defn- alter-watches [map op f & keys]
   (let [funcs (take (count keys) (repeat f))
 	kvs (interleave keys funcs)
-	watchers (watchers context)]
-    (with-meta context {:watchers (apply op watchers kvs)})))
+	watchers (watchers map)]
+    (with-meta map {:watchers (apply op watchers kvs)})))
 
 ;;; Public API
 
 (defn with-memo
-  "Merges the specified key-value pairs with the context's memo."
-  [context memo]
-  (with-meta context (merge (meta context) {:memo memo})))
+  "Merges the specified key-value pairs with the map's memo."
+  [map memo]
+  (with-meta map (merge (meta map) {:memo memo})))
 
 (defn memo
-  "Retrieves the memos attached to a context."
-  [context]
-  (:memo (meta context)))
+  "Retrieves the memos attached to a map."
+  [map]
+  (:memo (meta map)))
 
 (defn update 
-  "Updates the context with a new value. If silent is true, watchers are not
+  "Updates the map with a new value. If silent is true, watchers are not
   notified (defaults to false)."
-  ([context updates]
-     (update context updates false))
+  ([map updates]
+     (update map updates false))
 
-  ([context updates silent]
+  ([map updates silent]
      (let [initial-changes {:initial-changes updates}
-	   redundant? (some #(apply redundant-update? context %1) updates)]
+	   redundant? (some #(apply redundant-update? map %1) updates)]
        (cond
-	redundant? context
-	silent     (merge-with-meta context updates)
-	true       (merge context (without-memo
-				   (notify-watchers context
-						    (with-memo updates initial-changes))))))))
+	redundant? map
+	silent     (merge-with-meta map updates)
+	true       (merge map (without-memo
+			       (notify-watchers map
+						(with-memo updates initial-changes))))))))
 
 (defn touch
   "Runs handlers without modifying a value."
-  [context & keys]
-  (let [watchers (watchers-for-keys context keys)]
-    (run-watchers context context watchers)))
+  [map & keys]
+  (let [watchers (watchers-for-keys map keys)]
+    (run-watchers map map watchers)))
 
 (defn watch-keys
   "Adds a watch that is run only when the key's value changes. f should be a
-  function taking two arguments: the old context and the new context."
-  [context f & keys]
-  (apply alter-watches context multimap/add f keys))
+  function taking two arguments: the old map and the new map."
+  [map f & keys]
+  (apply alter-watches map multimap/add f keys))
 
 (defn unwatch-keys
   "Removes a watch from the specified keys."
-  [context f & keys]
-  (apply alter-watches context multimap/del f keys))
+  [map f & keys]
+  (apply alter-watches map multimap/del f keys))
--- test/spectator/core_test.clj
+++ test/spectator/core_test.clj
@@ -3,122 +3,122 @@
   (:use [clojure.test]))
 
 (deftest updates-key
-  (let [context {}
-	context (update context {:foo 1})]
-    (is (= (:foo context)
+  (let [map {}
+	map (update map {:foo 1})]
+    (is (= (:foo map)
 	   1))))
 
 (deftest stops-cycles-when-values-are-unchanged
-  (let [context {:a 0 :b 3}
-	context (watch-keys context (fn [old new] {:a (max 0 (dec (:b new)))}) :b)
-	context (watch-keys context (fn [old new] {:b (max 0 (dec (:a new)))}) :a)
-	context (update context {:a 3})]
-    (is (= (:a  context)
+  (let [map {:a 0 :b 3}
+	map (watch-keys map (fn [old new] {:a (max 0 (dec (:b new)))}) :b)
+	map (watch-keys map (fn [old new] {:b (max 0 (dec (:a new)))}) :a)
+	map (update map {:a 3})]
+    (is (= (:a  map)
 	   0))
-    (is (= (:b context)
+    (is (= (:b map)
 	   0))))
 
 (deftest watches-not-run-when-no-change
-  (let [context {:foo 1}
-	context (watch-keys context (fn [old new] {:ran true}) :foo)
-	context (update context {:foo 1})]
-    (is (not (:ran context)))))
+  (let [map {:foo 1}
+	map (watch-keys map (fn [old new] {:ran true}) :foo)
+	map (update map {:foo 1})]
+    (is (not (:ran map)))))
 
 (deftest watches-key
-  (let [context {}
-	context (watch-keys context (fn [old new] {:ran true}) :foo)
-	context (update context {:foo 1})]
-    (is (:ran context))))
+  (let [map {}
+	map (watch-keys map (fn [old new] {:ran true}) :foo)
+	map (update map {:foo 1})]
+    (is (:ran map))))
 
 (deftest watches-multiple-keys-with-one-watcher
-  (let [context {:m 0 :n 0}
-	context (watch-keys context (fn [old new] {:max (max (:m new) (:n new))}) :m :n)
-	context (update context { :m 1})]
-    (is (= (:max context)
+  (let [map {:m 0 :n 0}
+	map (watch-keys map (fn [old new] {:max (max (:m new) (:n new))}) :m :n)
+	map (update map { :m 1})]
+    (is (= (:max map)
 	   1))
     
-    (let [context (update context {:n 3})]
-      (is (= (:max context)
+    (let [map (update map {:n 3})]
+      (is (= (:max map)
 	     3)))))
 
 (deftest watches-multiple-keys-with-multiple-watchers
-  (let [context {}
-	context (watch-keys context (fn [old new] {:next-i (inc (:i new))}) :i)
-	context (watch-keys context (fn [old new] {:next-j (dec (:j new))}) :j)
-	context (update context {:i 3})
-	context (update context {:j 8})]
-    (is (= (:next-i context)
+  (let [map {}
+	map (watch-keys map (fn [old new] {:next-i (inc (:i new))}) :i)
+	map (watch-keys map (fn [old new] {:next-j (dec (:j new))}) :j)
+	map (update map {:i 3})
+	map (update map {:j 8})]
+    (is (= (:next-i map)
 	   4))
-    (is (= (:next-j context)
+    (is (= (:next-j map)
 	   7))))
 
 (deftest watches-one-key-with-multiple-watchers
-  (let [context {}
-	context (watch-keys context (fn [old new] {:next-i (inc (:i new))}) :i)
-	context (watch-keys context (fn [old new] {:prev-i (dec (:i new))}) :i)
-	context (update context {:i 3})]
-    (is (= (:next-i context)
+  (let [map {}
+	map (watch-keys map (fn [old new] {:next-i (inc (:i new))}) :i)
+	map (watch-keys map (fn [old new] {:prev-i (dec (:i new))}) :i)
+	map (update map {:i 3})]
+    (is (= (:next-i map)
 	   4))
-    (is (= (:prev-i context)
+    (is (= (:prev-i map)
 	   2))))
 
 (deftest gives-latest-watcher-priority
-  (let [context {}
-	context (watch-keys context (fn [old new] {:j (inc (:i new))}) :i)
-	context (watch-keys context (fn [old new] {:j (dec (:i new))}) :i)
-	context (update context {:i 3})]
-    (is (= (:j context)
+  (let [map {}
+	map (watch-keys map (fn [old new] {:j (inc (:i new))}) :i)
+	map (watch-keys map (fn [old new] {:j (dec (:i new))}) :i)
+	map (update map {:i 3})]
+    (is (= (:j map)
 	   2))))
 
 (deftest cascades-watcher-chains
-  (let [context {}
-	context (watch-keys context (fn [old new] {:b (inc (:a new))}) :a)
-	context (watch-keys context (fn [old new] {:c (inc (:b new))}) :b)
-	context (update context {:a 3})]
-    (is (= (:b context)
+  (let [map {}
+	map (watch-keys map (fn [old new] {:b (inc (:a new))}) :a)
+	map (watch-keys map (fn [old new] {:c (inc (:b new))}) :b)
+	map (update map {:a 3})]
+    (is (= (:b map)
 	   4))
-    (is (= (:c context)
+    (is (= (:c map)
 	   5))))
 
 (deftest removes-watches
-  (let [context {}
+  (let [map {}
 	f (fn [old new] {:ran true})
-	context (watch-keys context f :foo)
-	context (unwatch-keys context f :foo)
-	context (update context {:foo 1})]
-    (is (not (:ran context)))))
+	map (watch-keys map f :foo)
+	map (unwatch-keys map f :foo)
+	map (update map {:foo 1})]
+    (is (not (:ran map)))))
 
 (deftest removes-multiple-watches
-  (let [context {:count 0}
+  (let [map {:count 0}
 	f (fn [old new] {:count (inc (:count new))})
-	context (watch-keys context f :foo :bar :baz)
-	context (unwatch-keys context f :foo :bar)
-	context (update context {:foo 1})
-	context (update context {:bar 1})
-	context (update context {:baz 1})]
-    (is (= (:count context)
+	map (watch-keys map f :foo :bar :baz)
+	map (unwatch-keys map f :foo :bar)
+	map (update map {:foo 1})
+	map (update map {:bar 1})
+	map (update map {:baz 1})]
+    (is (= (:count map)
 	   1))))
 
 (deftest runs-watchers-without-changes-for-one-key
-  (let [context {:count 0}
-	context (watch-keys context (fn [old new] {:count (inc (:count new))}) :foo)
-	context (touch context :foo)]
-    (is (= (:count context)
+  (let [map {:count 0}
+	map (watch-keys map (fn [old new] {:count (inc (:count new))}) :foo)
+	map (touch map :foo)]
+    (is (= (:count map)
 	   1))))
 
 (deftest runs-one-watcher-without-changes-for-multiple-keys
-  (let [context {:count 0}
-	context (watch-keys context (fn [old new] {:count (inc (:count new))}) :foo :bar :baz)
-	context (touch context :foo :bar)]
-    (is (= (:count context)
+  (let [map {:count 0}
+	map (watch-keys map (fn [old new] {:count (inc (:count new))}) :foo :bar :baz)
+	map (touch map :foo :bar)]
+    (is (= (:count map)
 	   1))))
 
 (deftest runs-watchers-without-changes-for-multiple-keys
-  (let [context {:count 0}
-	context (watch-keys context (fn [old new] {:count (inc (:count new))}) :foo :baz)
-	context (watch-keys context (fn [old new] {:count (inc (:count new))}) :bar)
-	context (touch context :foo :bar)]
-    (is (= (:count context)
+  (let [map {:count 0}
+	map (watch-keys map (fn [old new] {:count (inc (:count new))}) :foo :baz)
+	map (watch-keys map (fn [old new] {:count (inc (:count new))}) :bar)
+	map (touch map :foo :bar)]
+    (is (= (:count map)
 	   2))))
 
 (deftest with-memo-adds-memo
@@ -126,36 +126,36 @@
 	 1)))
 
 (deftest updates-memo
-  (let [context {}
-	context (watch-keys context (fn [old new] (with-memo {} {:has-memo true})) :foo)
-	context (watch-keys context (fn [old new] {:has-memo (:has-memo (memo new))}) :foo)
-	context (update context {:foo 1})]
-    (is (:has-memo context))))
+  (let [map {}
+	map (watch-keys map (fn [old new] (with-memo {} {:has-memo true})) :foo)
+	map (watch-keys map (fn [old new] {:has-memo (:has-memo (memo new))}) :foo)
+	map (update map {:foo 1})]
+    (is (:has-memo map))))
 
 (deftest memo-stripped-after-watches
-  (let [context {}
-	context (watch-keys context (fn [old new] (with-memo {} {:has-memo true})) :foo)
-	context (update context {:foo 1})]
-    (is (not (memo context)))))
+  (let [map {}
+	map (watch-keys map (fn [old new] (with-memo {} {:has-memo true})) :foo)
+	map (update map {:foo 1})]
+    (is (not (memo map)))))
 
 (deftest memo-persists-through-watcher-chains
-  (let [context {}
-	context (watch-keys context (fn [old new] (with-memo {:bar 1} {:has-memo true})) :foo)
-	context (watch-keys context (fn [old new] {:baz 1}) :bar)
-	context (watch-keys context (fn [old new] {:has-memo (:has-memo (memo new))}) :baz)
-	context (update context {:foo 1})]
-    (is (:has-memo context))))
+  (let [map {}
+	map (watch-keys map (fn [old new] (with-memo {:bar 1} {:has-memo true})) :foo)
+	map (watch-keys map (fn [old new] {:baz 1}) :bar)
+	map (watch-keys map (fn [old new] {:has-memo (:has-memo (memo new))}) :baz)
+	map (update map {:foo 1})]
+    (is (:has-memo map))))
 
 (deftest includes-initial-changes
-  (let [context {}
-	context (watch-keys context (fn [old new] {:changes (:initial-changes (memo new))}) :foo)
-	context (update context {:foo 7})]
-    (is (= (:changes context)
+  (let [map {}
+	map (watch-keys map (fn [old new] {:changes (:initial-changes (memo new))}) :foo)
+	map (update map {:foo 7})]
+    (is (= (:changes map)
 	   {:foo 7}))))
 
-(deftest watchers-add-to-context
-  (let [context {:a 0 :b 1}
-	context (watch-keys context (fn [old new] {:d 3}) :c)
-	context (update context {:c 2})]
-    (is (= context
+(deftest watchers-add-to-map
+  (let [map {:a 0 :b 1}
+	map (watch-keys map (fn [old new] {:d 3}) :c)
+	map (update map {:c 2})]
+    (is (= map
 	   {:a 0 :b 1 :c 2 :d 3}))))
