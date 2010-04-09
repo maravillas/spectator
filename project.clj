652690b713a7b68750517a70a1f05c53eb9c2a86
Matthew Maravillas <maravillas@gmail.com>
2010-04-09 13:49:13 -0400

    Add lein-clojars dev dependency

--- project.clj
+++ project.clj
@@ -5,4 +5,6 @@
 		 [commons-logging/commons-logging "1.1"]]
   :dev-dependencies [[ant/ant "1.6.5"]
                      [ant/ant-launcher "1.6.5"]
-                     [leiningen/lein-swank "1.2.0-SNAPSHOT"]])
+                     [leiningen/lein-swank "1.2.0-SNAPSHOT"]
+		     [lein-clojars "0.5.0"]]
+  :repositories [["clojars" "http://clojars.org/repo"]])
