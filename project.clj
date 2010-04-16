d3dd4b2f20c3bd4b392f1698fcd0cbf925e5a534
Matthew Maravillas <maravillas@gmail.com>
2010-04-15 22:08:31 -0400

    Set version number to 1.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject spectator/spectator "1.1.0-SNAPSHOT"
+(defproject spectator/spectator "1.1.0"
   :description "A functional event system for Clojure maps."
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
