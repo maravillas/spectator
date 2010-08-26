1d9442a73f745ec7c042e57f867f8ad5bcfb5eaa
Matthew Maravillas <maravillas@gmail.com>
2010-08-26 00:36:35 -0400

    Increment to 1.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject spectator/spectator "1.2.0-SNAPSHOT"
+(defproject spectator/spectator "1.2.0"
   :description "A functional event system for Clojure maps."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
