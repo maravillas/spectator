17d579f7344bde3a0ecd988ce7f115a99d26a967
Matthew Maravillas <maravillas@gmail.com>
2010-08-26 00:33:24 -0400

    Update deps to 1.2 and clean up some dev-deps

--- project.clj
+++ project.clj
@@ -1,10 +1,8 @@
 (defproject spectator/spectator "1.2.0-SNAPSHOT"
   :description "A functional event system for Clojure maps."
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
 		 [commons-logging/commons-logging "1.1"]]
   :dev-dependencies [[ant/ant "1.6.5"]
                      [ant/ant-launcher "1.6.5"]
-                     [leiningen/lein-swank "1.2.0-SNAPSHOT"]
-		     [lein-clojars "0.5.0"]]
-  :repositories [["clojars" "http://clojars.org/repo"]])
+                     [swank-clojure "1.2.1"]])
