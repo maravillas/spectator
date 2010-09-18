17af6c70a874e8d08c63dd306a66307405df5340
Matthew Maravillas <maravillas@gmail.com>
2010-09-17 22:47:11 -0400

    Split core-test into updaters and observers

--- test/spectator/core_updaters_test.clj
+++ test/spectator/core_updaters_test.clj
@@ -1,4 +1,4 @@
-(ns spectator.core-test
+(ns spectator.core-updaters-test
   (:use [spectator.core] :reload-all)
   (:use [clojure.test]))
 
@@ -147,17 +147,6 @@
     (is (= (:i map)
 	   4))))
 
-(deftest runs-observers-when-touching-multiple-keys
-  (let [ref (ref 0)
-	agent (agent nil)
-	map (-> {:foo 0 :bar 0}
-		(add-observer (fn [old new diff] (dosync (commute ref inc))) :foo :baz)
-		(add-observer (fn [old new diff] (dosync (commute ref inc))) :bar)
-		(touch [:foo :bar] :agent agent))]
-    (await agent)
-    (is (= @ref
-	   2))))
-
 (deftest runs-chained-watchers-when-touching
   (let [map (-> {:count 0}
 		(add-updater (fn [old new diff] {:bar 1 :count (inc (:count new))}) :foo)
@@ -267,111 +256,6 @@
 		(update {:foo true}))]
     (is (:meta (meta map)))))
 
-(deftest runs-observers
-  (let [atom (atom false)
-	agent (agent nil)
-	map (-> {}
-		(add-updater (fn [old new diff] {:pure true}) :foo)
-		(add-observer (fn [old new diff] (swap! atom (fn [_] true))) :foo)
-		(update {:foo true} :agent agent))]
-    (await agent)
-    (is @atom)))
-
-(deftest ignores-impure-results
-  (let [atom (atom false)
-	agent (agent nil)
-	map (-> {:ignored true}
-		(add-observer (fn [old new diff] {:ignored false}) :foo)
-		(update {:foo true} :agent agent))]
-    (await agent)
-    (is (:ignored map))))
-
-(deftest provides-observers-with-contexts
-  (let [atom (atom {})
-	agent (agent nil)
-	map (-> {:foo false :bar true}
-		(add-observer (fn [old new diff] (swap! atom (fn [_] {:old old :new new}))) :foo)
-		(update {:foo true} :agent agent))]
-    (await agent)
-    (is (= (:new @atom)
-	   {:foo true :bar true}))
-    (is (= (:old @atom)
-	   {:foo false :bar true}))))
-
-(deftest runs-observers-for-changed-key-only
-  (let [ref1 (ref false)
-	ref2 (ref false)
-	agent (agent nil)
-	map (-> {}
-		(add-observer (fn [old new diff] (dosync (ref-set ref1 true))) :foo)
-		(add-observer (fn [old new diff] (dosync (ref-set ref2 true))) :bar)
-		(update {:foo true} :agent agent))]
-    (await agent)
-    (is @ref1)
-    (is (not @ref2))))
-
-(deftest runs-pure-watchers-before-observers
-  (let [ref (ref {})
-	agent (agent nil)
-	map (-> {:foo false :bar false}
-		(add-updater (fn [old new diff] {:bar true}) :foo)
-		(add-observer (fn [old new diff] (dosync (ref-set ref new))) :foo)
-		(update {:foo true} :agent agent))]
-    (await agent)
-    (is (= @ref
-	   {:foo true :bar true}))))
-
-(deftest runs-observers-when-touching
-  (let [ref (ref false)
-	agent (agent nil)
-	map (-> {}
-		(add-observer (fn [old new diff] (dosync (ref-set ref true))) :foo)
-		(touch :foo :agent agent))]
-    (await agent)
-    (is @ref)))
-
-(deftest provides-memo-to-observers
-  (let [ref (ref {})
-	agent (agent nil)
-	map (-> {}
-		(add-observer (fn [old new diff] (dosync (ref-set ref (memo new)))) :foo)
-		(update {:foo true} :memo {:a true} :agent agent))]
-    (await agent)
-    (is (= @ref
-	   {:initial-changes {:foo true} :a true}))))
-
-(deftest passes-updater-memos-to-observers
-  (let [ref (ref {})
-	agent (agent nil)
-	map (-> {}
-		(add-updater (fn [old new diff] (with-memo {} {:updater true})) :foo)
-		(add-observer (fn [old new diff] (dosync (ref-set ref (memo new)))) :foo)
-		(update {:foo true} :agent agent))]
-    (await agent)
-    (is (= @ref
-	   {:initial-changes {:foo true} :updater true}))))
-
-(deftest provides-memo-to-observers-without-changes
-  (let [ref (ref {})
-	agent (agent nil)
-	map (-> {}
-		(add-updater (fn [old new diff] (with-memo {} {:extra-memo true})) :foo)
-		(add-observer (fn [old new diff] (dosync (ref-set ref (memo new)))) :foo)
-		(touch :foo :agent agent))]
-    (await agent)
-    (is (= @ref
-	   {:extra-memo true}))))
-
-(deftest provides-original-memo-to-observers-without-changes
-  (let [ref (ref {})
-	agent (agent nil)
-	map (-> {}
-		(add-observer (fn [old new diff] (dosync (ref-set ref (memo new)))) :foo)
-		(touch :foo :memo {:bar true} :agent agent))]
-    (await agent)
-    (is (= @ref
-	   {:bar true}))))
-
 (deftest runs-global-updaters
   (let [map (-> {:foo 0 :bar 0 :baz 0}
 		(add-updater (fn [old new diff] (reduce (fn [all key]
@@ -394,30 +278,6 @@
     (is (= (:sum map)
 	   1))))
 
-(deftest runs-global-observers
-  (let [ref (ref 0)
-	agent (agent nil)
-	map (-> {}
-		(add-observer (fn [old new diff] (dosync (commute ref inc))))
-		(update {:foo 1} :agent agent)
-		(update {:bar 2 :baz 3} :agent agent))]
-    (await agent)
-    (is (= @ref
-	   2))))
-
-(deftest removes-global-observer
-  (let [ref (ref 0)
-	f (fn [old new diff] (dosync (commute ref inc)))
-	agent (agent nil)
-	map (-> {}
-		(add-observer f)
-		(update {:foo 1} :agent agent)
-		(remove-observer f)
-		(update {:bar 2 :baz 3} :agent agent))]
-    (await agent)
-    (is (= @ref
-	   1))))
-
 (deftest specifies-key-diff-to-updaters
   (let [map (-> {:foo 0 :bar 1 :baz 2}
 		(add-updater (fn [old new diff] {:diff1 diff}) :foo)
@@ -429,22 +289,6 @@
     (is (= (:diff2 map)
 	   [:bar :baz]))))
 
-(deftest specifies-key-diff-to-observers
-  (let [ref (ref {})
-	agent (agent nil)
-	map (-> {:foo 0 :bar 1 :baz 2}
-		(add-observer (fn [old new diff] (dosync (commute ref merge {:diff1 diff})))
-			      :foo)
-		(add-observer (fn [old new diff] (dosync (commute ref merge {:diff2 diff})))
-			      :bar)
-		(update {:foo 1} :agent agent)
-		(update {:bar 2 :baz 3} :agent agent))]
-    (await agent)
-    (is (= (:diff1 @ref)
-	   [:foo]))
-    (is (= (sort (:diff2 @ref))
-	   (sort [:bar :baz])))))
-
 (deftest specifies-only-recent-diff-to-updaters
   (let [map (-> {}
 		(add-updater (fn [old new diff] {:b (inc (:a new))}) :a)
@@ -452,16 +296,3 @@
 		(update {:a 3}))]
     (is (= (:diff map)
 	   [:b]))))
-
-(deftest specifies-entire-diff-to-observers
-  (let [ref (ref {})
-	agent (agent nil)
-	map (-> {}
-		(add-updater (fn [old new diff] {:b (inc (:a new))}) :a)
-		(add-updater (fn [old new diff] {:c (inc (:b new))}) :b)
-		(add-observer (fn [old new diff] (dosync (commute ref merge {:diff diff})))
-			      :b)
-		(update {:a 3} :agent agent))]
-    (await agent)
-    (is (= (sort (:diff @ref))
-	   (sort [:a :b :c])))))
